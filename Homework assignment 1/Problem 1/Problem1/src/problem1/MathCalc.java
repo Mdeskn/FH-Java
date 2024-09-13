b1e2d744d7be25e8362c667b7e41a35ff313db2f
mdeskn <maed.es74@gmail.com>
2024-09-13 06:15:05 +0330

    Added README file and documented Problem 1

--- Homework assignment 1/Problem 1/Problem1/src/problem1/MathCalc.java
+++ Homework assignment 1/Problem 1/Problem1/src/problem1/MathCalc.java
@@ -2,22 +2,22 @@ package problem1;
 
 public class MathCalc {
     public static void main(String[] args) {
-        // Step 1: Define the diameters of the Earth and Sun
-        double earthDiameter = 7600.0;  // in miles
-        double sunDiameter = 865000.0;  // in miles
+        // First we have to define the diameters of the Earth and Sun
+        double earthDiameter = 7600.0;
+        double sunDiameter = 865000.0;
         
-        // Step 2: Calculate the radius (radius = diameter / 2)
+        // then we need to calculate the radius (radius = diameter / 2)
         double earthRadius = earthDiameter / 2;
         double sunRadius = sunDiameter / 2;
         
-        // Step 3: Use the volume formula: (4/3) * pi * r^3
+        // then we have to use the volume formula: (4/3) * pi * r^3
         double earthVolume = (4.0 / 3.0) * Math.PI * Math.pow(earthRadius, 3);
         double sunVolume = (4.0 / 3.0) * Math.PI * Math.pow(sunRadius, 3);
         
-        // Step 4: Calculate the ratio of the volume of the Sun to the Earth
+        // and then calculate the ratio of the volume of the Sun to the Earth
         double volumeRatio = sunVolume / earthVolume;
         
-        // Step 5: Output the results
+        // the last step is to output the results
         System.out.println("The volume of the Earth is " + earthVolume + " cubic miles.");
         System.out.println("The volume of the Sun is " + sunVolume + " cubic miles.");
         System.out.println("The ratio of the volume of the Sun to the volume of the Earth is " + volumeRatio);
